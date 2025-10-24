f16fd655c1dc40826752cb8ca9b427b0146f35c2
Frank McCrudden <fmccrudden@cps.edu>
2025-10-24 10:36:00 -0500

    started a4

--- notes.py
+++ notes.py
@@ -12,7 +12,7 @@ class Dog:
         favorite_food - the dog's favorite food
     """
 
-    def __init__(self, fur_color, name, age, favorite_food):
+    def __init__(self, fur_color = "black", name = "no name", age = "1", favorite_food = "kibble"):
         """Initialize a new dog with fur_color, name, age,
         and favorite_food
         """
@@ -26,12 +26,28 @@ class Dog:
     def bark(self):
         """Make the dog bark"""
         return f"{self.name} says Woof, Woof!!"
+    def birthday(self):
+        """Celebrate the dog's birthday"""
+        self.age += 1
+        print(f"Celebrating {self.name}'s birthday! They are now {self.age} years old")
+    def change_favorite_food(self, new_food):
+        """Change the favorite food of the dog"""
+        old_food = self.favorite_food
+        self.favorite_food = new_food
+        print(f"{self.name} use to love {old_food} and now loves {self.favorite_food}")
         
 
 my_dog = Dog("black", "logan", 9, "salmon")
 enggy_dog = Dog("black and white", "pelchin", 13, "rice")
-
+defualt_dog = Dog()
+aaron_dog = Dog("peach and white", "Dumbo", 1, "anything edible")
+ 
 print(my_dog)
 print(enggy_dog)
 print()
 print(enggy_dog.bark)
+print()
+print(defualt_dog)
+print(aaron_dog)
+aaron_dog.birthday
+print(aaron_dog)
